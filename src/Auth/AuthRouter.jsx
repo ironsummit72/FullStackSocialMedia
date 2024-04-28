8bee86f3985104f3f235e57d134b4f12face26b3
ironsummit72 <ironsumit72@gmail.com>
2024-04-28 20:12:50 +0530

    protected Routing added

--- src/Auth/AuthRouter.jsx
+++ src/Auth/AuthRouter.jsx
@@ -1,9 +1,18 @@
 
-
-function AuthRouter() {
-  return (
-    <div>AuthRouter</div>
-  )
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+function AuthRouter({ children }) {
+  const auth = useSelector((state) => state.isUserAuthenticated);
+  return auth ? (
+    <div>{children}</div>
+  ) : (
+    <div>
+      please login{" "}
+      <Link to="/login" className="text-blue-600">
+        login
+      </Link>
+    </div>
+  );
 }
 
-export default AuthRouter
+export default AuthRouter;
