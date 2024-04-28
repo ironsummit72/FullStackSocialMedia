3cd8a28ea3b735b0dfcc7e20ad0e8d820d3df470
ironsummit72 <ironsumit72@gmail.com>
2024-04-28 15:32:50 +0530

    register page with form validations

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,157 @@
-
-
+import { Input } from "@/shadcomponents/ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/shadcomponents/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { RegisterFormSchema } from "@/validation/ZodValidation";
+import { Button } from "@/shadcomponents/ui/button";
+import { Link } from "react-router-dom";
+import { axiosInstance } from "@/axios/axiosInstance";
 function Register() {
+  const form = useForm({
+    resolver: zodResolver(RegisterFormSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      firstname: "",
+      lastname: "",
+      password: "",
+      cpassword: "",
+    },
+  });
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <div>Register</div>
-  )
+    <section className="bg-gray-50">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+              Create an account
+            </h1>
+            <Form {...form}>
+              <form
+                onSubmit={form?.handleSubmit(onSubmit)}
+                className="space-y-5"
+              >
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Username</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter Your username" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your public username.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter Your email" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your email address
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="firstname"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>First Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter Your firstname" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your public Firstname
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="lastname"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Last Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter Your lastname" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your public Lastname of your full name
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter your password" {...field} />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="cpassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Confirm password</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Confirm Password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button className="w-full" type="submit">
+                  Register
+                </Button>
+                <p className="text-sm font-light text-gray-500">
+                  Don’t have an account yet?{" "}
+                  <Link
+                    to="/login"
+                    className="font-medium text-blue-600 hover:underline "
+                  >
+                    Login
+                  </Link>
+                </p>
+              </form>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Register
+export default Register;
