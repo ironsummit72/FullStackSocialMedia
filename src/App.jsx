df5ac974f3b9fe9b01fdc2765b90702b0c2c6f6d
ironsummit72 <ironsumit72@gmail.com>
2024-04-28 20:11:45 +0530

    outlet added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-
+import { useEffect } from "react";
+import { axiosInstanceWithCredentials } from "./axios/axiosInstance";
+import { useDispatch } from "react-redux";
+import { authenticateUser, logout } from "./redux/AuthSlice/AuthSlice";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    <div>App</div>
-  )
+  const dispatch = useDispatch();
+  useEffect(() => {
+    axiosInstanceWithCredentials
+      .get("/getcurrentuser")
+      .then((res) => {
+        console.log(res.data.data);
+        dispatch(authenticateUser(res.data.data));
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(logout());
+      });
+  }, []);
+  return <Outlet/>;
 }
 
-export default App
+export default App;
