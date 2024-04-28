f7f87ca3ec31388590065cba62fbe4da33896757
ironsummit72 <ironsumit72@gmail.com>
2024-04-29 01:16:51 +0530

    protected Router added

--- src/Auth/AuthRouter.jsx
+++ src/Auth/AuthRouter.jsx
@@ -1,18 +1,12 @@
-
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import {  Navigate, } from "react-router-dom";
 function AuthRouter({ children }) {
   const auth = useSelector((state) => state.isUserAuthenticated);
-  return auth ? (
-    <div>{children}</div>
-  ) : (
-    <div>
-      please login{" "}
-      <Link to="/login" className="text-blue-600">
-        login
-      </Link>
-    </div>
-  );
+  console.log(auth);
+  if (auth) {
+    return <div>{children}</div>;
+  }
+  return <Navigate to={"/login"} replace={true} />;
 }
 
 export default AuthRouter;
