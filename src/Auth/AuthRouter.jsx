bd4f1b55245c8a077c8575ce7e23332252adf6d5
ironsummit72 <ironsumit72@gmail.com>
2024-04-29 01:17:09 +0530

    removed unessessery logs

--- src/Auth/AuthRouter.jsx
+++ src/Auth/AuthRouter.jsx
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux";
 import {  Navigate, } from "react-router-dom";
 function AuthRouter({ children }) {
   const auth = useSelector((state) => state.isUserAuthenticated);
-  console.log(auth);
   if (auth) {
     return <div>{children}</div>;
   }
