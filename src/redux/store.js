fb6d32757fffba6f2038caf01dd19ef398b4f026
ironsummit72 <ironsumit72@gmail.com>
2024-04-29 12:14:18 +0530

    removed todo comment

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./AuthSlice/AuthSlice";
-// TODO: import reducers here
+
 
 const store=configureStore({
     reducer:authSlice
