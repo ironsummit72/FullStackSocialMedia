9348a065d72cceb7791cd99a837841f382a6b8ae
ironsummit72 <ironsumit72@gmail.com>
2024-05-01 16:21:46 +0530

    feat(routes): Add routes for uploading displaypicture and coverpicture
    - Added new routes to handle user uploads for `displaypicture` and `coverpicture`.
    - These routes allow users to upload and update their profile and cover images.
    - Included necessary middleware for file handling, such as Multer, to manage the upload process.
    - Ensures users can personalize their profiles with appropriate images.

---
+++ backend/routes/set.routes.js
@@ -0,0 +1,43 @@
+import Router from 'express'
+const router = Router()
+import multer from '../middlewares/multer.middleware.js'
+import userModel from '../models/users.models.js'
+import ApiResponse from '../utils/ApiResponse.util.js'
+router.post('/dp', multer.single('displaypicture'), async (req, res) => {
+	if (req.user) {
+		const data = await userModel.findOne({username: req.user.username})
+		if (data) {
+			const updatedData = await userModel.updateOne(
+				{username: req.user.username},
+				{$set: {displaypicture: req.file.filename}},
+			)
+			if (updatedData) {
+				res.status(200).json(new ApiResponse('success', 200, null, 'user display picture set successfully', null))
+			}
+		} else {
+			res.status(400).json(new ApiResponse('failed', 400, null, 'user not found', null))
+		}
+	} else {
+		res.status.apply(401).json(new ApiResponse('failed', 401, null, 'user not found please login', null))
+	}
+})
+router.post('/cover', multer.single('coverpicture'), async (req, res) => {
+	if (req.user) {
+		const data = await userModel.findOne({username: req.user.username})
+		if (data) {
+			const updatedData = await userModel.updateOne(
+				{username: req.user.username},
+				{$set: {coverpicture: req.file.filename}},
+			)
+			if (updatedData) {
+				res.status(200).json(new ApiResponse('success', 200, null, 'user coverpicture set successfully', null))
+			}
+		} else {
+			res.status(400).json(new ApiResponse('failed', 400, null, 'user not found', null))
+		}
+	} else {
+		res.status.apply(401).json(new ApiResponse('failed', 401, null, 'user not found please login', null))
+	}
+})
+
+export default router
