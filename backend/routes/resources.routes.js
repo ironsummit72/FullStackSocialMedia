fd396023e4d7c167e9b492afa5ffdbedc2ff370e
ironsummit72 <ironsumit72@gmail.com>
2024-05-03 16:27:29 +0530

    feat(user): Allow users to retrieve displaypicture and coverpicture
    - Added functionality for users to retrieve their `displaypicture` and `coverpicture`.
    - This allows users to view their profile and cover images through the application.
    - Updated routes and controllers to return the appropriate image data when requested.
    - Facilitates a more interactive and personalized user experience.

---
+++ backend/routes/resources.routes.js
@@ -0,0 +1,58 @@
+import ApiResponse from '../utils/ApiResponse.util.js'
+import userModel from '../models/users.models.js'
+import Router from 'express'
+import fs from 'fs'
+const router = Router()
+
+router.get('/displaypicture', async (req, res) => {
+	const {username} = req.user
+	if (username) {
+		const userData = await userModel.findOne({username})
+		const readableStream = fs.createReadStream(`./uploads/displaypicture/${userData.displaypicture}`)
+		readableStream.on('data', (chunk) => res.write(chunk))
+		readableStream.on('end', () => res.end())
+		readableStream.on('error', (err) => res.json(new ApiResponse('error', 400, err.message, 'file not found', null)))
+	} else {
+		res.status(400).json(new ApiResponse('error', 400, null, 'usernotfound', null))
+	}
+})
+router.get('/displaypicture/:username', async (req, res) => {
+	const {username} = req.params
+	if (username) {
+		const userData = await userModel.findOne({username})
+		if (userData) {
+			const readableStream = fs.createReadStream(`./uploads/displaypicture/${userData.displaypicture}`)
+			readableStream.on('data', (chunk) => res.write(chunk))
+			readableStream.on('end', () => res.end())
+			readableStream.on('error', (err) => res.json(new ApiResponse('error', 400, err.message, 'file not found', null)))
+		}
+	} else {
+		res.status(400).json(new ApiResponse('error', 400, null, 'please provide username', null))
+	}
+})
+router.get('/coverpicuture', async (req, res) => {
+	const {username} = req.user
+	if (username) {
+		const userData = await userModel.findOne({username})
+		const readableStream = fs.createReadStream(`./uploads/coverpicture/${userData.coverpicture}`)
+		readableStream.on('data', (chunk) => res.write(chunk))
+		readableStream.on('end', () => res.end())
+		readableStream.on('error', (err) => res.json(new ApiResponse('error', 400, err.message, 'file not found', null)))
+	} else {
+		res.status(400).json(new ApiResponse('error', 400, null, 'usernotfound', null))
+	}
+})
+router.get('/coverpicuture/:username', async (req, res) => {
+	const {username} = req.params
+	if (username) {
+		const userData = await userModel.findOne({username})
+		const readableStream = fs.createReadStream(`./uploads/coverpicture/${userData.coverpicture}`)
+		readableStream.on('data', (chunk) => res.write(chunk))
+		readableStream.on('end', () => res.end())
+		readableStream.on('error', (err) => res.json(new ApiResponse('error', 400, err.message, 'file not found', null)))
+	} else {
+		res.status(400).json(new ApiResponse('error', 400, null, 'usernotfound', null))
+	}
+})
+
+export default router
