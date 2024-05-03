d228a4b9b2dffd471f361c9f2e3e85f70c3fb97d
ironsummit72 <ironsumit72@gmail.com>
2024-05-04 00:21:10 +0530

    added a sidenavbar

---
+++ src/components/SideNavBar.jsx
@@ -0,0 +1,47 @@
+import { Button } from "@/shadcomponents/ui/button";
+import { Link } from "react-router-dom";
+import DisplayPicture from "@/components/DisplayPicture";
+import { Bookmark, MonitorPlay, Users, Gamepad2 } from "lucide-react";
+import { useSelector } from "react-redux";
+
+function SideNavBar() {
+    const state=useSelector(state=>state.userData);
+  return (
+    <div className="w-96 h-screen bg-white sticky top-0 z-10">
+      <ul className= "space-y-10">
+      <li className="hover:bg-gray-300 w-[90%] rounded-md p-2 flex items-center">
+          <Button asChild variant="ghost" className="w-full">
+            <Link
+              className="w-full flex gap-5 p-6 text-lg font-semibold hover:bg-gray-300"
+              to="/ownprofile">
+              <DisplayPicture className={`w-10 h-10 rounded-full`} />
+              {state?.username}
+            </Link>
+          </Button>
+        </li>
+        <li className="hover:bg-gray-300 w-[90%] rounded-md p-2 flex items-center">
+          <Button asChild variant="ghost" className="w-full">
+            <Link
+              className="w-full flex gap-5 p-6 text-lg font-semibold hover:bg-gray-300"
+              to="/saved">
+               <Bookmark fill='red'/>
+              Saved
+            </Link>
+          </Button>
+        </li>
+        <li className="hover:bg-gray-300 w-[90%] rounded-md p-2 flex items-center">
+          <Button asChild variant="ghost" className="w-full">
+            <Link
+              className="w-full flex gap-5 p-6 text-lg font-semibold hover:bg-gray-300"
+              to="/watch">
+               <MonitorPlay color='green'/>
+              Video
+            </Link>
+          </Button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default SideNavBar;
