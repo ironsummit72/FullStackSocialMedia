16bfdf4ced261a71b6d614eb0ed3e318ac799289
ironsummit72 <ironsumit72@gmail.com>
2024-05-04 01:23:24 +0530

    feat(navbar): Add logout functionality to dropdown menu
    - Implemented logout functionality within the dropdown menu in the Navbar component.
    - Users can now log out directly from the Navbar, improving accessibility and user flow.
    - This change provides a convenient way for users to end their session, enhancing overall usability.

--- src/components/Dropdown/ProfileDropDown.jsx
+++ src/components/Dropdown/ProfileDropDown.jsx
@@ -1,48 +1,57 @@
+import { axiosInstanceWithCredentials } from "@/axios/axiosInstance";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-    DropdownMenuShortcut
-  } from '@/shadcomponents/ui/dropdown-menu';
-  import { User, Settings, LogOut } from 'lucide-react';
-  import { useNavigate } from 'react-router-dom';
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuShortcut,
+} from "@/shadcomponents/ui/dropdown-menu";
+import { User, Settings, LogOut } from "lucide-react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 
-  
-  function ProfileDropDown({ children }) {
-    const navigate = useNavigate();
+function ProfileDropDown({ children }) {
+  const navigate = useNavigate();
+  const state=useSelector(state=>state.userData)
+  const onLogoutHandler = () => {
+    axiosInstanceWithCredentials.delete("/auth/logout").then((response) => {
+      toast(response.data.message, { description: `bye see you again, ${state?.username}` });
+      setTimeout(() => {
+        navigate(response.data.redirectUrl, { replace: true });
+      }, 2000);
+    });
+  };
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <User className="mr-2 h-4 w-4" />
+          <span>Profile</span>
+          <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Settings className="mr-2 h-4 w-4" />
+          <span>Settings</span>
+          <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={onLogoutHandler}>
+          <LogOut className="mr-2 h-4 w-4 text-red-500" />
+          <span className="text-red-500">Log out</span>
+          <DropdownMenuShortcut className="text-red-500">
+            ⇧⌘Q
+          </DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
 
-    const onLogoutHandler = () => {
-      console.log('logout');
-    };
-    return (
-      <DropdownMenu>
-        <DropdownMenuTrigger>{children}</DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <User className="mr-2 h-4 w-4" />
-            <span>Profile</span>
-            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Settings className="mr-2 h-4 w-4" />
-            <span>Settings</span>
-            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={onLogoutHandler}>
-            <LogOut className="mr-2 h-4 w-4 text-red-500" />
-            <span className="text-red-500">Log out</span>
-            <DropdownMenuShortcut className="text-red-500">⇧⌘Q</DropdownMenuShortcut>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    );
-  }
-  
-  export default ProfileDropDown;
+export default ProfileDropDown;
