4e1b35e1cd8ded7431377988c15164712494ff33
ironsummit72 <ironsumit72@gmail.com>
2024-05-04 17:09:53 +0530

    feat(feed): Add StoryContainer to Feed component
    - Integrated the `StoryContainer` into the `Feed` component to display stories within the feed.
    - This addition allows users to view and interact with stories directly from the feed section.
    - Enhances the feed's functionality by including story-related content in a cohesive manner.

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,13 +1,20 @@
 import SideNavBar from "@/components/SideNavBar";
 import Container from "@/components/Container";
+import CreatePostCard from "@/components/CreatePostCard";
+import StoryContainer from "@/components/StoryContainer";
 
 
 
 function Feed() {  
-return <div className="bg-red-500 w-full h-auto min-h-screen flex">
+return <div className="bg-gray-100 w-screen h-auto min-h-screen flex overflow-x-hidden">
 <SideNavBar/>
 <Container>
+<StoryContainer>
+{/* // add logic to show stories of other users who they follow */}
 
+
+</StoryContainer>
+<CreatePostCard/>
 </Container>
   </div>;
 
