c8f8ae7389eea619fbc8eec3e669e8a3c09d480a
ironsummit72 <ironsumit72@gmail.com>
2024-05-04 12:25:50 +0530

    feat(component): Add CoverPicture component to display user cover images
    - Created a new CoverPicture component for displaying a user's cover picture.
    - This component can be used to showcase the cover image on user profiles or other relevant sections of the application.
    - Improves the visual representation of user profiles and offers flexibility for customization.

---
+++ src/components/CoverPicture.jsx
@@ -0,0 +1,31 @@
+import  { useEffect, useState } from 'react';
+import { AspectRatio } from '@/shadcomponents/ui/aspect-ratio';
+import { axiosInstanceWithCredentials } from '@/axios/axiosInstance';
+
+function CoverPicture({className,username}) {
+    const [CoverPicture,setCoverPicture]=useState('');
+    useEffect(() => {
+        if (username) {
+          axiosInstanceWithCredentials
+            .get(`resource/coverpicture/${username}`, { responseType: "blob" })
+            .then((response) => {
+              setCoverPicture(URL.createObjectURL(response.data));
+            });
+        } else {
+          axiosInstanceWithCredentials
+            .get(`resource/coverpicture`, { responseType: "blob" })
+            .then((response) => {
+              setCoverPicture(URL.createObjectURL(response.data));
+            });
+        }
+      }, [username]);
+  return (
+    <div className="w-full h-[70%] ">
+      <AspectRatio className={`flex justify-center h-full mt-20 ${className}`} ratio={16 / 9}>
+        <video poster={CoverPicture} alt="Image" className="rounded-md object-cover w-[80%] h-[65%] object-[top_right]" />
+      </AspectRatio>
+    </div>
+  );
+}
+
+export default CoverPicture;
