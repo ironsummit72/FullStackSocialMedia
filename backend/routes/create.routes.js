9bd6490ea41fba915839caa1029d7fd6c836b0b3
ironsummit72 <ironsumit72@gmail.com>
2024-05-04 19:14:11 +0530

    feat(route): Add postVisibility field to create post route
    - Updated the create post route to include a `postVisibility` field, allowing users to specify the visibility of a new post.
    - This change supports setting visibility levels for posts, such as public, private, or friends-only.
    - Enables more control and flexibility when creating posts, enhancing user privacy and content management.

--- backend/routes/create.routes.js
+++ backend/routes/create.routes.js
@@ -9,10 +9,9 @@ const router = Router()
 
 // check if user is logged in or not
 
-router.post('/post', multerUpload.array('posts', 5), async (req, res) => {
-	console.log(req.files)
-	const {caption} = req.body
-	const postData = await postModel.create({caption, user: req?.user.id, media: req.files})
+router.post('/post', multerUpload.array('posts', 10),async(req, res) => {
+	const {caption,postvisibility} = req.body
+	const postData = await postModel.create({caption, user: req?.user.id, media: req.files,postvisibility})
 	res.json(new ApiResponse('success', 200, postData, 'user post data', null))
 })
 
