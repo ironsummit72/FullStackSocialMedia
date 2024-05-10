0b6bd8c17e09315a099a527066c17eb67b708192
ironsummit72 <ironsumit72@gmail.com>
2024-05-11 00:50:31 +0530

    feat(react-query): Add suggestUser query function for user suggestions in captions
    - Created a new query function called `suggestUser` to provide user suggestions while writing captions.
    - This function fetches and returns user data based on input, helping users find and mention other users in their captions.
    - Enhances interactivity by enabling real-time user suggestions during content creation.

--- src/api/QueryFunctions.js
+++ src/api/QueryFunctions.js
@@ -8,3 +8,10 @@ export function searchUserQuery(searchText)
     return fetchData
 }
 
+export function suggestUser(searchText)
+{
+    console.log("searchText",searchText);
+    let fetchData= axiosInstanceWithCredentials.get(`/search/mention?search=${searchText}`).then((res)=>res.data.data)
+    return fetchData
+}
+
