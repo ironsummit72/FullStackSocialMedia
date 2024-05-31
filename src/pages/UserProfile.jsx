47143a0382c370b216a77b48dda40b1f9eeb286f
ironsummit72 <ironsumit72@gmail.com>
2024-05-31 13:56:23 +0530

    feat(page): Add UserProfile page displaying comprehensive user information
    - Implemented the UserProfile page to showcase detailed user information.
    - Displays user profile elements including displayPicture, coverPicture, reels, photos, posts, info, videos, friends, followers, and following.
    - Enhances user engagement by providing a complete view of a user's profile and activities.

---
+++ src/pages/UserProfile.jsx
@@ -0,0 +1,44 @@
+import { getUserDetails } from "@/api/QueryFunctions";
+import CoverPicture from "@/components/CoverPicture";
+import DisplayPicture from "@/components/DisplayPicture";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useParams, Link, NavLink ,Outlet} from "react-router-dom";
+
+function UserProfile() {
+  const { username } = useParams();
+  const { data } = useQuery({
+    queryKey: ["userprofile", username],
+    queryFn: ({ queryKey }) => getUserDetails(queryKey[1]),
+  });
+  return (
+    <>
+      <div className="w-screen h-screen">
+       <div className="con w-full h-[90%] bg-white shadow-2xl">
+        <CoverPicture username={data?.username}/>
+        <DisplayPicture username={data?.username} className="w-64 h-64 rounded-full relative left-64 border-4 border-white"/>
+        <div className="info relative left-[25%] bottom-[12%] w-fit flex flex-col gap-3 ">
+          <div className="headings flex items-center gap-4"><h1 className="name text-2xl font-bold">{data?.firstname} {data?.lastname}</h1><Link to={`../${data?.username}`}><span className="font-semibold text-gray-500">@{data?.username}</span></Link></div>
+          <div className="followersInfo flex gap-10 w-fit ">
+            <Link to={`/${data?.username}/followers`}>Followers</Link>
+            <Link to={`/${data?.username}/following`}>Following</Link>
+
+          </div>
+        </div>
+       </div>
+       <nav className="bg-white w-full h-20 flex items-center relative shadow-2xl ">
+       <div className="navitems flex gap-x-20 px-72">
+       <NavLink to={`/${data?.username}`}><span className="font-bold text-gray-600">Posts</span></NavLink>
+        <NavLink to={`/${data?.username}/about`}><span className="font-bold text-gray-600">About</span></NavLink>
+        <NavLink to={`/${data?.username}/reels`}><span className="font-bold text-gray-600">Reels</span></NavLink>
+        <NavLink to={`/${data?.username}/photos`}><span className="font-bold text-gray-600">Photos</span></NavLink>
+        <NavLink to={`/${data?.username}/videos`}><span className="font-bold text-gray-600">Videos</span></NavLink>
+        <NavLink to={`/${data?.username}/friends`}><span className="font-bold text-gray-600">Friends</span></NavLink>
+       </div>
+       </nav>
+       <Outlet/>
+      </div>
+    </>
+  );
+}
+
+export default UserProfile;
