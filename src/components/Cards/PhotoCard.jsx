a5c1a200d05139a96bcccec2b1b1ecd108c14c97
ironsummit72 <ironsumit72@gmail.com>
2024-05-31 19:33:06 +0530

    feat(component): Add PhotosCard component for previewing user photos
    - Developed a PhotosCard component to display a preview of the first 9 user photos.
    - Provides a quick visual summary of the user's photo gallery.
    - Enhances the user profile by showcasing recent or popular photos in a compact format.

---
+++ src/components/Cards/PhotoCard.jsx
@@ -0,0 +1,20 @@
+
+import { Link } from "react-router-dom"
+
+function PhotoCard({username}) {
+  return (
+    <div className='relative w-[25%]   bg-white rounded-lg shadow-2xl mr-20 px-10 py-4 mt-10'>
+   <div className="headercon flex items-center justify-between">
+   <h1 className="Intro font-bold text-black text-lg  ">Photos</h1>
+   <Link  to={`/${username}/photos`} className="text-blue-500 font-semibold">See All Photos</Link>
+   </div>
+   <div className="grid grid-rows-3 grid-cols-3 gap-3 mt-3">
+    {/* TODO:Loop the user photos here not more than 9  with links to the photo id  */}
+
+   </div>
+   
+</div>
+  )
+}
+
+export default PhotoCard
