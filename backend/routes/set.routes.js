f605fa60ad76dad0bdf971d0de92ed5fd942180a
ironsummit72 <ironsumit72@gmail.com>
2024-06-01 17:04:19 +0530

    feat(routes): Add profileIntro route to set routes for updating profile intro details
    - Created a new route in set.routes.js named profileIntro to handle updates for profession, studiedAt, wentTo, livesIn, and relationshipStatus fields.
    - This route enables users to modify their profile introduction information, enhancing profile customization and personalization.

--- backend/routes/set.routes.js
+++ backend/routes/set.routes.js
@@ -33,7 +33,28 @@ router.post('/cover', multer.single('coverpicture'), async (req, res) => {
 				res.status(200).json(new ApiResponse('success', 200, null, 'user coverpicture set successfully', null))
 			}
 		} else {
-			res.status(400).json(new ApiResponse('failed', 400, null, 'user not found', null))
+			res.status(400).json(new ApiResponse('failed', 404, null, 'user not found', null))
+		}
+	} else {
+		res.status.apply(401).json(new ApiResponse('failed', 401, null, 'user not found please login', null))
+	}
+})
+router.post('/profileintro', async (req, res) => {
+	const {profession, studiedat, wentto, livesin, relationshipstatus} = req.body
+	if (req.user) {
+		const data = await userModel.findOne({username: req.user.username})
+		if (data) {
+			const dbResponse = await userModel.updateOne(
+				{username: req.user.username},
+				{
+					$set: {profession, studiedAt: studiedat, wentTo: wentto, livesIn: livesin, RelationshipStatus: relationshipstatus},
+				},
+			)
+			if (dbResponse) {
+				res.status(200).json(new ApiResponse('success', 200, null, 'user intro details updated successfully', null))
+			}
+		} else {
+			res.status(404).json(new ApiResponse('failed', 404, null, 'user not found', null))
 		}
 	} else {
 		res.status.apply(401).json(new ApiResponse('failed', 401, null, 'user not found please login', null))
