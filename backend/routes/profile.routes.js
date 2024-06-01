4179a49797cc62af05a90b869610747d0675a081
ironsummit72 <ironsumit72@gmail.com>
2024-06-01 20:26:15 +0530

    feat(api): Add /profile/introdetails route to fetch user intro details
    - Implemented a new route `/profile/introdetails` in the Express server.
    - This route retrieves and returns user introduction details such as profession, studiedAt, wentTo, livesIn, and relationshipStatus.
    - Allows clients to access and display user intro information efficiently.

--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -24,6 +24,21 @@ router.get('/user',async(req,res)=>{
     }
 
 })
+router.get('/introdetails', async (req, res) => {
+	try {
+		const {username} = req.query
+		if (username) {
+			const userData = await userModel.findOne({username}).select('profession studiedAt wentTo livesIn RelationshipStatus')
+			if (userData) {
+				res.status(200).json(new ApiResponse('success', 200, userData, 'user intro details', null))
+			} else {
+				res.status(404).json(new ApiResponse('success', 404, userData, 'userprofile details', null))
+			}
+		}
+	} catch (error) {
+		console.error(error)
+	}
+})
 
 
 export default router;
