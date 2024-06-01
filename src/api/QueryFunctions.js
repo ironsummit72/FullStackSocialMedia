f055065740860d61c940db7621ae1b031e239cdf
ironsummit72 <ironsumit72@gmail.com>
2024-06-02 00:14:21 +0530

    feat(queries): Add getUserIntroDetails query function
    - Introduced a new query function `getUserIntroDetails` to fetch user introduction details.
    - This function facilitates retrieving information such as profession, studiedAt, wentTo, livesIn, and relationshipStatus.
    - Enhances data fetching capabilities in the application for displaying user profile intro details.

--- src/api/QueryFunctions.js
+++ src/api/QueryFunctions.js
@@ -4,23 +4,25 @@ export function searchUserQuery(searchText)
 {
     console.log("searchText",searchText);
     let fetchData= axiosInstanceWithCredentials.get(`/search/users?search=${searchText}`).then((res)=>res.data.data)
-    return fetchData
+    return fetchData;
 }
-
 export function suggestUser(searchText)
 {
     console.log("searchText",searchText);
     let fetchData= axiosInstanceWithCredentials.get(`/search/mention?search=${searchText}`).then((res)=>res.data.data)
-    return fetchData
+    return fetchData;
 }
 export function getUserDetails(username){
     console.log('username in QueryFunction',username);
     let fetchData= axiosInstanceWithCredentials.get(`/profile/user?username=${username}`).then((res)=>res.data.data)
-    return fetchData
-
+    return fetchData;
 }
 export function setUserDetails(postData){
 let postDataResponse=axiosInstanceWithCredentials.post('/set/profileintro',postData);
 return postDataResponse;
+}
+export function getUserIntroDetails(username){
+    let fetchData= axiosInstanceWithCredentials.get(`/profile/introdetails?username=${username}`).then((res)=>res.data.data)
+    return fetchData;
 }
 
