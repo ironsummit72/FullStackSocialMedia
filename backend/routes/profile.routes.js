7ec5410c08f411ced6b8ffd8b32cab6e6fc5528a
ironsummit72 <ironsumit72@gmail.com>
2024-06-07 23:36:03 +0530

    feat(api): Shuffle and send followers and following lists
    - Integrated the shuffle method to randomize the order of followers and following lists before sending them in the response.
    - Ensures that the followers and following data are presented in a randomized order for a more dynamic user experience.
    - Enhances the display of social connections by providing a varied presentation each time the data is fetched.

--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -1,28 +1,23 @@
-import userModel from "../models/users.models.js";
-import ApiResponse from "../utils/ApiResponse.util.js";
-import Router from 'express';
-const router=Router();
+import shuffle from '../utils/Shuffle.js'
+import userModel from '../models/users.models.js'
+import ApiResponse from '../utils/ApiResponse.util.js'
+import Router from 'express'
+const router = Router()
 
-
-router.get('/user',async(req,res)=>{
-    try {
-        const {username}=req.query;
-        if(username)
-     {
-      const userData=await userModel.findOne({username}).select(['-password'])
-      if(userData)
-        {
-            res.status(200).json(new ApiResponse('success',200,userData,'userprofile details',null));
-        }else{
-
-            res.status(404).json(new ApiResponse('success',404,userData,'userprofile details',null));
-        }
-     }
-        
-    } catch (error) {
-        console.error(error);
-    }
-
+router.get('/user', async (req, res) => {
+	try {
+		const {username} = req.query
+		if (username) {
+			const userData = await userModel.findOne({username}).select(['-password'])
+			if (userData) {
+				res.status(200).json(new ApiResponse('success', 200, userData, 'userprofile details', null))
+			} else {
+				res.status(404).json(new ApiResponse('success', 404, userData, 'userprofile details', null))
+			}
+		}
+	} catch (error) {
+		console.error(error)
+	}
 })
 router.get('/introdetails', async (req, res) => {
 	try {
@@ -39,30 +34,27 @@ router.get('/introdetails', async (req, res) => {
 		console.error(error)
 	}
 })
-router.get('/followers/:username',async (req,res)=>{
-const username=req.params.username;
-if(username)
-    {
-        const userData=await userModel.findOne({username}).populate({path:'followers',select:'-password'})
-        if(userData){
-            res.status(200).json(new ApiResponse("success",200,userData.followers,`followers of ${username}`,null));
-            }else{
-            res.status(200).json(new ApiResponse("success",200,userData.followers,`no followers of ${username}`,null));
-        }
-    }
+router.get('/followers/:username', async (req, res) => {
+	const username = req.params.username
+	if (username) {
+		const userData = await userModel.findOne({username}).populate({path: 'followers', select: '-password'})
+		if (userData) {
+			res.status(200).json(new ApiResponse('success', 200, shuffle(userData.followers), `followers of ${username}`, null))
+		} else {
+			res.status(200).json(new ApiResponse('success', 200, userData.followers, `no followers of ${username}`, null))
+		}
+	}
 })
-router.get('/following/:username',async (req,res)=>{
-const username=req.params.username;
-if(username)
-    {
-        const userData=await userModel.findOne({username}).populate({path:'following',select:'-password'})
-        if(userData){
-            res.status(200).json(new ApiResponse("success",200,userData.following,`following of ${username}`,null));
-            }else{
-            res.status(200).json(new ApiResponse("success",200,userData.following,`no following of ${username}`,null));
-        }
-    }
+router.get('/following/:username', async (req, res) => {
+	const username = req.params.username
+	if (username) {
+		const userData = await userModel.findOne({username}).populate({path: 'following', select: '-password'})
+		if (userData) {
+			res.status(200).json(new ApiResponse('success', 200, shuffle(userData.following), `following of ${username}`, null))
+		} else {
+			res.status(200).json(new ApiResponse('success', 200, userData.following, `no following of ${username}`, null))
+		}
+	}
 })
 
-
-export default router;
+export default router
