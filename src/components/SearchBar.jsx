734ab64eae820a464190749f7789ab8b58ec4d57
ironsummit72 <ironsumit72@gmail.com>
2024-06-08 14:17:23 +0530

    fix(search): Improve search bar behavior and suggestions visibility
    - Updated the search bar to hide the suggestion dropdown when there is no input text.
    - Enhanced the search functionality to automatically hide suggestions and clear the search bar when a result is clicked.
    - Improves user experience by ensuring the search suggestions are only visible when relevant and clearing the input after selection.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,31 +5,40 @@ import { Link } from "react-router-dom";
 import useDebounce from "@/hooks/useDebouncing";
 import { useQuery } from "@tanstack/react-query";
 import { searchUserQuery } from "@/api/QueryFunctions";
+import { useRef } from "react";
 function SearchBar() {
   const [searchValue, setSearchValue] = useState("");
+  const searchRef=useRef()
   const debounce = useDebounce(searchValue, 700);
   const { data,isSuccess } = useQuery({
     queryKey: ["search", debounce],
     queryFn: ({ queryKey }) => searchUserQuery(queryKey[1]),
-  });
+    });
+  const [showSuggestions,setShowSuggestions] = useState(isSuccess);
   return (
     <div className="">
       <div className="searchContainer mb-2">
-        <Input
+        <Input ref={searchRef}
           className="w-72 relative top-1 rounded-full bg-gray-300/50"
           placeholder="Search Users"
           type="search"
           onChange={(e) => {
             setSearchValue(e.target.value);
+            if(e.target.value.length > 0) {
+              setShowSuggestions(true)
+            }else{
+              setShowSuggestions(false)
+            }
           }}
         />
-        {isSuccess && (
+        {showSuggestions && (
           <div className="suggessioncard bg-white  w-72 min-h-20 max-h-96 shadow-md rounded-b-md overflow-y-auto absolute top-14 ">
             {data?.slice(0,5).map((data) => {
               return (
                 <Link
                   key={data._id}
                   to={`/${data.username}`}
+                  onClick={()=>{setShowSuggestions(false),searchRef.current.value=""}}
                   className="searchSuggestion flex items-center gap-3 mb-4 mt-4 hover:bg-gray-200/50"
                 >
                   <DisplayPicture
