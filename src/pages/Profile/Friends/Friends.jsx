21df8c310f8ec93fcb02371702ead86460b266c3
ironsummit72 <ironsumit72@gmail.com>
2024-06-09 01:08:58 +0530

    refactor: Move Friends.jsx to pages/Friends/Friend.jsx
    - Moved the Friends.jsx file to a new directory at pages/Friends/Friend.jsx.
    - Organized the project structure for better file management and maintainability.
    - Enhances codebase clarity by grouping related components and pages together.

---
+++ src/pages/Profile/Friends/Friends.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FriendsCard from './components/FriendsCard'
+
+function Friends() {
+  return (
+    <div className="w-full flex justify-center mt-10">
+      <FriendsCard/>
+    </div>
+  )
+}
+
+export default Friends
