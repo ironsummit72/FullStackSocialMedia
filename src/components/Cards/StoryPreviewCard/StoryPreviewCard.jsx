a9054a3bf4a354305458163471e69b8f9d01197c
ironsummit72 <ironsumit72@gmail.com>
2024-07-23 14:23:16 +0530

    feat(frontend): Add StoryPreviewCard component
    - Created a new component StoryPreviewCard.jsx.
    - This card displays an image preview of the story or a video thumbnail if the story is a video.
    - Note: Video thumbnail feature is not included in this commit and will be added in a future update.

---
+++ src/components/Cards/StoryPreviewCard/StoryPreviewCard.jsx
@@ -0,0 +1,39 @@
+import { Card } from "@/shadcomponents/ui/card";
+import DisplayPicture from "@/components/DisplayPicture";
+import { useQuery } from "@tanstack/react-query";
+import { getStoriesOfUser } from "@/api/QueryFunctions";
+import StoryPreviewPhoto from "./StoryPreviewPhoto";
+import { Link } from "react-router-dom";
+function StoryPreviewCard({ className, username }) {
+  const { data } = useQuery({
+    queryKey: ["story", username],
+    queryFn: ({ queryKey }) => getStoriesOfUser(queryKey[1]),
+    enabled: !!username,
+  });
+  console.log('story data preview card',data);
+  if (data[0]) {
+    return (
+      <Link to={`stories/${username}`}>
+        <Card className={` max-w-40 min-w-40 ${className}`}>
+          {/* className=" shadow-xl rounded-md ml-5 transition ease-in-out delay-150  hover:scale-105 cursor-pointer p-0"> */}
+          <div className="h-full w-full flex flex-col justify-between">
+            <DisplayPicture
+              className="w-10 h-10 rounded-full z-10 absolute border-blue-600 border-4"
+              username={username}
+            />
+            <StoryPreviewPhoto
+              className={" object-fill"}
+              filename={data[data.length-1]?.content?.filename}
+              disablelink={true}
+            />
+            <span className="font-semibold text-sm z-30 text-white">{data[0]?.user.firstname} {data[0]?.user.lastname}</span>
+          </div>
+        </Card>
+      </Link>
+    );
+  } else {
+    <></>;
+  }
+}
+
+export default StoryPreviewCard;
