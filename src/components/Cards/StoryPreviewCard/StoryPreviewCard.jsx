9a2472b6ce1bda808afff4c51c8b369092ed7514
ironsummit72 <ironsumit72@gmail.com>
2024-07-23 14:32:12 +0530

    fix(frontend): Add check for undefined data in StoryPreviewCard
    - Added an if statement to check if the data is not undefined in StoryPreviewCard.jsx.
    - Ensures that the component handles cases where data may be missing or undefined, preventing potential errors.

--- src/components/Cards/StoryPreviewCard/StoryPreviewCard.jsx
+++ src/components/Cards/StoryPreviewCard/StoryPreviewCard.jsx
@@ -10,22 +10,23 @@ function StoryPreviewCard({ className, username }) {
     queryFn: ({ queryKey }) => getStoriesOfUser(queryKey[1]),
     enabled: !!username,
   });
-  console.log('story data preview card',data);
-  if (data[0]) {
-    return (
-      <Link to={`stories/${username}`}>
+  if(data)
+  {
+    if (data[0]) {
+      return (
+        <Link to={`stories/${username}`}>
         <Card className={` max-w-40 min-w-40 ${className}`}>
           {/* className=" shadow-xl rounded-md ml-5 transition ease-in-out delay-150  hover:scale-105 cursor-pointer p-0"> */}
           <div className="h-full w-full flex flex-col justify-between">
             <DisplayPicture
               className="w-10 h-10 rounded-full z-10 absolute border-blue-600 border-4"
               username={username}
-            />
+              />
             <StoryPreviewPhoto
               className={" object-fill"}
               filename={data[data.length-1]?.content?.filename}
               disablelink={true}
-            />
+              />
             <span className="font-semibold text-sm z-30 text-white">{data[0]?.user.firstname} {data[0]?.user.lastname}</span>
           </div>
         </Card>
@@ -35,5 +36,8 @@ function StoryPreviewCard({ className, username }) {
     <></>;
   }
 }
+}
+
+
 
 export default StoryPreviewCard;
