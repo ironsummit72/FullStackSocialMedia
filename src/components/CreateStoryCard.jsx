4e5b291db9de690d908447d173d7058c51347abb
ironsummit72 <ironsumit72@gmail.com>
2024-07-23 14:18:23 +0530

    feat(frontend): Integrate shadcn Card component and add create story dialog
    - Updated CreateStoryCard.jsx to use the shadcn Card component.
    - Added a create story dialog that opens when the add button is clicked.
    - Enhances the UI by incorporating the shadcn Card component and providing an intuitive story creation experience.

--- src/components/CreateStoryCard.jsx
+++ src/components/CreateStoryCard.jsx
@@ -1,20 +1,25 @@
 import DisplayPicture from "./DisplayPicture";
 import { Plus } from "lucide-react";
 import { Button } from "@/shadcomponents/ui/button";
-import { Link } from "react-router-dom";
+import { Card, CardContent } from "@/shadcomponents/ui/card";
+import CreateStoryDialog from "./Dialogs/CreateStoryDialog/CreateStoryDialog";
 function CreateStoryCard() {
   return (
-    <div className="max-w-40 min-w-40 h-72 bg-gray-100 shadow-xl rounded-md ml-5 transition ease-in-out delay-150  hover:scale-105 cursor-pointer ">
-      <DisplayPicture className={"rounded-t-md"} />
-      <Button
-        aschild
-        className="rounded-full h-14 w-14 relative left-12 bottom-7 border-4 border-white">
-        <Link to="/createstory">
-          <Plus />
-        </Link>
-      </Button>
-      <span className="font-bold relative top-10 right-12">Create Story</span>
-    </div>
+    <Card>
+      <CardContent className="max-w-40 min-w-40 h-72 shadow-xl rounded-md ml-5 transition ease-in-out delay-150  hover:scale-105 cursor-pointer p-0">
+        <div className="w-full flex flex-col">
+          <DisplayPicture className="rounded-t-md hover:rounded-t-md" />
+          <CreateStoryDialog> 
+          <Button
+            className="rounded-full h-14 w-14 relative left-12 bottom-7 border-4 border-white"
+            >
+              <Plus />
+          </Button>
+            </CreateStoryDialog>
+          <span className="font-semibold">Create Story</span>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
 export default CreateStoryCard;
