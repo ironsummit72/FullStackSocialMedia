2639b43ddc3f7e8e581c8958359291fd3dd36a17
ironsummit72 <ironsumit72@gmail.com>
2024-07-26 19:41:55 +0530

    feat: Add routes for handling story views
    - Added GET route `/stories/sid/:storyId` to retrieve story data by story ID.
    - Added POST route `/stories/addviews/:storyId` to add logged-in user ID to the views field in stories model when a story is viewed.
    - Added GET route `/stories/showviews/:storyId` to return views data from the story database.

--- backend/routes/stories.routes.js
+++ backend/routes/stories.routes.js
@@ -10,17 +10,31 @@ router.get('/:username', async function (req, res) {
 	const {username} = req.params
 	if (username) {
 		// import the stories model and find the stories of specific user
-        const userData=await userModel.findOne({username});
-        if(userData)
-        {
-            const storyData=await StoryModel.find({user:userData._id}).populate({path:'user',select:'-password'});
-            if(storyData){
-                res.status(200).json(new ApiResponse(200,'success',storyData,`stories of ${username}`,null));
-            }else{
-                res.status(404).json(new ApiResponse(404,'success',storyData,`no stories of ${username}`,null));
-            }
-        }
-
+		const userData = await userModel.findOne({username})
+		if (userData) {
+			const storyData = await StoryModel.find({user: userData._id}).populate({path: 'user', select: '-password'})
+			if (storyData) {
+				res.status(200).json(new ApiResponse(200, 'success', storyData, `stories of ${username}`, null))
+			} else {
+				res.status(404).json(new ApiResponse(404, 'success', storyData, `no stories of ${username}`, null))
+			}
+		}
+	}
+})
+router.get('/sid/:storyId', async function (req, res) {
+	const {storyId} = req.params
+	if (storyId) {
+		try {
+			const storyData = await StoryModel.findById(storyId).populate({path: 'user', select: '-password'})
+			if (storyData) {
+				res.status(200).json(new ApiResponse('success', 200, storyData, 'stories data', null))
+			} else {
+				res.status(200).json(new ApiResponse('success', 200, storyData, 'stories data', null))
+			}
+		} catch (err) {
+			res.status(400).json(new ApiResponse('bad request', 400, err, 'stories data', null))
+			console.error(err)
+		}
 	}
 })
 router.post('/create', multerUpload.single('story'), async function (req, res) {
@@ -37,7 +51,50 @@ router.post('/create', multerUpload.single('story'), async function (req, res) {
 			}
 		}
 	}
-});
+})
 
+router.post('/addviews/:storyId', async function (req, res) {
+	const {storyId} = req.params
+	const loggedInUser = req?.user.username
+	console.log('story Id ', storyId)
+	if (storyId && loggedInUser) {
+		try {
+			const storyData = await StoryModel.findById(storyId.trim())
+			const userData = await userModel.findOne({username: loggedInUser})
+			if (storyData && userData) {
+				if (storyData.user !== userData._id) {
+					if (storyData.views.indexOf(userData._id) === -1) {
+						storyData.views.push(userData._id)
+						await storyData.save()
+						res
+							.status(200)
+							.json(new ApiResponse('success', 200, null, `this ${storyData._id} is viewed by ${userData.id}`, null))
+					} else {
+						res.status(200).json(new ApiResponse('success', 200, null, `already viewed`, null))
+					}
+				} else {
+					res.status(200).json(new ApiResponse('success', 200, null, `loggin user viewing the story`, null))
+				}
+			} else {
+				res.status(404).json(new ApiResponse('not found', 404, null, `storydata or userData not found`, null))
+			}
+		} catch (error) {
+			console.error(error)
+		}
+	}
+})
 
+router.get('/showviews/:storyId', async function (req, res) {
+	const {storyId} = req.params
+	if (storyId) {
+		try {
+			const storyData = await StoryModel.findById(storyId).populate({path: 'views', select: '-password'}).select('-caption ')
+			res.status(200).json(new ApiResponse('success', 200, storyData, 'storyviews', null))
+		} catch (error) {
+			console.error(error)
+		}
+	} else {
+		res.status(404).json(new ApiResponse('not found', 404, null, 'story not found', null))
+	}
+})
 export default router
