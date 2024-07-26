4d60a6b5d5c3a22aed766bc29246d11eccde1f23
ironsummit72 <ironsumit72@gmail.com>
2024-07-26 20:08:13 +0530

    feat: Update StoryPreviewCard to display "Your Story" for logged-in user with video/photo preview functionality

--- src/components/Cards/StoryPreviewCard/StoryPreviewCard.jsx
+++ src/components/Cards/StoryPreviewCard/StoryPreviewCard.jsx
@@ -4,12 +4,16 @@ import { useQuery } from "@tanstack/react-query";
 import { getStoriesOfUser } from "@/api/QueryFunctions";
 import StoryPreviewPhoto from "./StoryPreviewPhoto";
 import { Link } from "react-router-dom";
+import StoryPreviewVideo from "./StoryPreviewVideo";
+import { useSelector } from "react-redux";
+
 function StoryPreviewCard({ className, username }) {
   const { data } = useQuery({
     queryKey: ["story", username],
     queryFn: ({ queryKey }) => getStoriesOfUser(queryKey[1]),
     enabled: !!username,
   });
+  const loggedInusername = useSelector((state) => state.userData?.username);
   if(data)
   {
     if (data[0]) {
@@ -22,12 +26,19 @@ function StoryPreviewCard({ className, username }) {
               className="w-10 h-10 rounded-full z-10 absolute border-blue-600 border-4"
               username={username}
               />
-            <StoryPreviewPhoto
-              className={" object-fill"}
-              filename={data[data.length-1]?.content?.filename}
-              disablelink={true}
-              />
-            <span className="font-semibold text-sm z-30 text-white">{data[0]?.user.firstname} {data[0]?.user.lastname}</span>
+              {
+                 data[data.length-1]?.content?.mimetype.split('/')[0]==='image'?  <StoryPreviewPhoto
+                  className={" object-fill"}
+                  filename={data[data.length-1]?.content?.filename}
+                  disablelink={true}
+                  />: <StoryPreviewVideo
+                  className={" object-fill"}
+                  filename={data[data.length-1]?.content?.filename}
+                  disablelink={true}
+                  />   
+              }
+            
+            {loggedInusername===username?<span className="font-semibold text-sm z-30 text-white">Your Story</span>:<span className="font-semibold text-sm z-30 text-white">{data[0]?.user.firstname} {data[0]?.user.lastname}</span>}
           </div>
         </Card>
       </Link>
