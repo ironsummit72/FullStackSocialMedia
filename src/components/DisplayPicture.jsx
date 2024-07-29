bf576aee9fc9167e6d7ec233a78f2131951674d6
ironsummit72 <ironsumit72@gmail.com>
2024-07-29 20:55:19 +0530

    feat: Add Gradient Border to DisplayPicture for Users with Stories
    - Enhanced DisplayPicture component to show a gradient border if the user has stories.
    - Introduced a new prop 'showStoryBorder' to conditionally display the story border.
    - Updated logic to check story availability and apply the gradient border accordingly.

--- src/components/DisplayPicture.jsx
+++ src/components/DisplayPicture.jsx
@@ -1,24 +1,38 @@
-import { axiosInstanceWithCredentials } from "@/axios/axiosInstance";
 import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { getDisplayPicture } from "@/api/QueryFunctions";
+import { getDisplayPicture, getHasStory } from "@/api/QueryFunctions";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-function DisplayPicture({ username, className }) {
+function DisplayPicture({ username, className,showStoryBorder, }) {
   const [displaypicture, setDisplayPicture] = useState("null");
+  const {data:hasstorydata}=useQuery({queryKey:['hastory',username],queryFn:({queryKey})=>getHasStory(queryKey[1]),enabled:!!username})
   const loggedInuser=useSelector((state)=>state.userData?.username)
   const { data } = useQuery({ queryKey: ["displaypicture", username? username:loggedInuser],queryFn:({queryKey})=>getDisplayPicture(queryKey[1]),enabled:!!loggedInuser});
+
   useEffect(()=>{
    if(data)
     {
       setDisplayPicture(URL.createObjectURL(data))
     }
   },[data])
-  return (
-    <div>
-      <img className={className} src={displaypicture} alt="" />
-    </div>
-  );
+
+
+  if(hasstorydata)
+  {
+    return (
+      <Link to={`/stories/${username}`} >
+        <img className={`${className} ${hasstorydata===true && showStoryBorder==true?'gradientborder-story':''}`} src={displaypicture} alt="" />
+      </Link>
+    );
+  }else{
+    return (
+      <div >
+        <img className={`${className} ${hasstorydata===true && showStoryBorder==true?'gradientborder-story':''}`} src={displaypicture} alt="" />
+      </div>
+    );
+  }
+
 }
 
 export default DisplayPicture;
