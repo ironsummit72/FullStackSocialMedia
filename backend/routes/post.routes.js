a4076958fe84a425fff46e8e8c6b49ec8b381f67
ironsummit72 <ironsumit72@gmail.com>
2024-07-31 13:15:03 +0530

    feat: Add Delete Post Route
    - Added a DELETE route in post.routes.js.
    - Allows the owner of a post to delete their post using the postId.

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -2,10 +2,11 @@ import postModel from '../models/post.models.js'
 import ApiResponse from '../utils/ApiResponse.util.js'
 import {Router} from 'express'
 const router = Router()
+
 router.get('/:id', async (req, res) => {
 	const {id} = req.params
 	if (id) {
-		const postData = await postModel.findOne({_id: id}).populate('user').exec();
+		const postData = await postModel.findOne({_id: id}).populate('user').exec()
 		if (postData) {
 			res.status(200).send(new ApiResponse('success', 200, postData, 'post data', null))
 		} else {
@@ -13,4 +14,22 @@ router.get('/:id', async (req, res) => {
 		}
 	}
 })
+
+router.delete('/:postId', async (req, res) => {
+	const {postId} = req.params
+	if (postId) {
+		const postData = await postModel.findById(postId)
+		if (postData.user.toString() === req.user.id) {
+			const postDeleteData = await postModel.findByIdAndDelete(postId)
+			if (postDeleteData) {
+				res.status(200).send(new ApiResponse('success', 200, null, 'post deleted successfully', null))
+			} else {
+				res.status(400).send(new ApiResponse('bad request', 400, null, 'something went wrong', null))
+			}
+		} else {
+			res.status(401).send(new ApiResponse('unauthorized', 401, null, 'you do not have access to delete this post', null))
+		}
+	}
+})
+
 export default router
